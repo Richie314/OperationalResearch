7202900cd9800bfe8b570ded6871ceb636a70885
Richie314 <richieinleghorn@gmail.com>
2024-01-20 19:59:44 +0100

    Rename to english

--- RicercaOperativa/Properties/Settings.Designer.cs
+++ RicercaOperativa/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace RicercaOperativa.Properties {
+namespace OperationalResearch.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
